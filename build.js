f16f8fd117aa4453d01ac6f7f2b02499d4c79892
Robinih <138433392+Robinih@users.noreply.github.com>
2024-11-15 22:21:55 +0800

    Update build.js

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 const header = document.querySelector("header");
 
 window.addEventListener ("scroll", function() {
-  header.classList.toggle ("sticky", window.scrollY > 200)});
+  header.classList.toggle ("sticky", window.scrolly > 200)});
 
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
